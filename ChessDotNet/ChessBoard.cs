379ab2962d18382507a414b5ea6d343298aaaeef
ProgramFOX <programfox@hotmail.be>
2015-07-19 18:46:29 +0200

    [WIP] Add basic move validation & applying
    Still todo:
     - Take castling in account
     - Take en passant in account
     - Validate that you won't put yourself in check
     - Validate that other pieces are not in the way to do your move

--- ChessDotNet/ChessBoard.cs
+++ ChessDotNet/ChessBoard.cs
@@ -50,5 +50,73 @@ namespace ChessDotNet
                 { rw, nw, bw, qw, kw, bw, nw, rw }
             };
         }
+
+        public ChessPiece GetPieceAt(Position.Files file, Position.Ranks rank)
+        {
+            return Board[(int)rank, (int)file];
+        }
+
+        protected void SetPieceAt(Position.Files file, Position.Ranks rank, ChessPiece cp)
+        {
+            Board[(int)rank, (int)file] = cp;
+        }
+
+        public bool IsValidMove(Move m)
+        {
+            if (m.OriginalPosition.Equals(m.NewPosition))
+                return false;
+            ChessPiece piece = GetPieceAt(m.OriginalPosition.File, m.OriginalPosition.Rank);
+            if (piece.Player != m.Player) return false;
+            PositionDelta posDelta = new PositionDelta(m.OriginalPosition, m.NewPosition);
+            switch (piece.Piece)
+            {
+                case Pieces.King:
+                    if ((posDelta.DeltaX != 1 || posDelta.DeltaY != 1)
+                        && (posDelta.DeltaX != 0 || posDelta.DeltaY != 1)
+                        && (posDelta.DeltaX != 1 || posDelta.DeltaY != 0))
+                        return false; // TODO: take castling in account
+                    break;
+                case Pieces.Pawn:
+                    if (posDelta.DeltaX != 0 || posDelta.DeltaY != 1)
+                        return false;
+                    if (piece.Player == Players.White && (int)m.OriginalPosition.Rank < (int)m.NewPosition.Rank)
+                        return false;
+                    if (piece.Player == Players.Black && (int)m.OriginalPosition.Rank > (int)m.NewPosition.Rank)
+                        return false;
+                    // TODO: take capturing in account
+                    break;
+                case Pieces.Queen:
+                    if (posDelta.DeltaX != posDelta.DeltaY && posDelta.DeltaX != 0 && posDelta.DeltaY != 0)
+                        return false;
+                    break;
+                case Pieces.Rook:
+                    if (posDelta.DeltaX != 0 && posDelta.DeltaY != 0)
+                        return false;
+                    break;
+                case Pieces.Bishop:
+                    if (posDelta.DeltaX != posDelta.DeltaY)
+                        return false;
+                    break;
+                case Pieces.Knight:
+                    if ((posDelta.DeltaX != 2 || posDelta.DeltaY != 1) && (posDelta.DeltaX != 1 || posDelta.DeltaY != 2))
+                        return false;
+                    break;
+                default:
+                    return false;
+            }
+            // TODO: Validate that you are not going to put yourself in check
+            // TODO: Validate that other pieces are not in the way to do your move
+            return true;
+        }
+
+        public bool ApplyMove(Move m, bool alreadyValidated)
+        {
+            if (!alreadyValidated && !IsValidMove(m))
+                return false;
+            ChessPiece movingPiece = GetPieceAt(m.OriginalPosition.File, m.OriginalPosition.Rank);
+            SetPieceAt(m.NewPosition.File, m.NewPosition.Rank, movingPiece);
+            SetPieceAt(m.OriginalPosition.File, m.OriginalPosition.Rank, ChessPiece.None);
+            return true;
+        }
     }
 }
---
+++ ChessDotNet/PositionDelta.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessDotNet
+{
+    public struct PositionDelta
+    {
+        public int DeltaX
+        {
+            get;
+            private set;
+        }
+
+        public int DeltaY
+        {
+            get;
+            private set;
+        }
+        public PositionDelta(Position p1, Position p2)
+        {
+            DeltaX = Math.Abs((int)p1.File - (int)p2.File);
+            DeltaY = Math.Abs((int)p1.Rank - (int)p2.Rank);
+        }
+    }
+}
