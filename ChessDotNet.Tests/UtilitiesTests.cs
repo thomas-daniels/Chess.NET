e8fb6e746f8c05aee56af047c9fd512c8e5430fa
ProgramFOX <programfox@hotmail.be>
2015-08-30 13:39:56 +0200

    Add tests for Utilities class

---
+++ ChessDotNet.Tests/UtilitiesTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChessDotNet;
+using NUnit.Framework;
+
+namespace ChessDotNet.Tests
+{
+    [TestFixture]
+    public class UtilitiesTests
+    {
+        [Test]
+        public void TestThrowIfNull()
+        {
+            Assert.Throws<ArgumentNullException>(delegate ()
+            {
+                object value = null;
+                Utilities.ThrowIfNull(value, "value");
+            });
+
+            Assert.DoesNotThrow(delegate ()
+            {
+                ChessPiece piece = new ChessPiece(Piece.Bishop, Player.White);
+                Utilities.ThrowIfNull(piece, "piece");
+            });
+        }
+
+        [Test]
+        public void TestGetOpponentOf()
+        {
+            Assert.AreEqual(Player.Black, Utilities.GetOpponentOf(Player.White));
+            Assert.AreEqual(Player.White, Utilities.GetOpponentOf(Player.Black));
+            Assert.Throws<ArgumentException>(delegate ()
+            {
+                Player p = Utilities.GetOpponentOf(Player.None);
+            });
+        }
+    }
+}
