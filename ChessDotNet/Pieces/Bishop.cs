dabc692f3cf22ac1250126143c3c925c930f39b0
ProgramFOX <programfox@hotmail.be>
2016-01-22 20:30:47 +0100

    Implement IsValidDestination for Bishop

--- ChessDotNet/Pieces/Bishop.cs
+++ ChessDotNet/Pieces/Bishop.cs
@@ -17,5 +17,27 @@
         {
             return Owner == Player.White ? "B" : "b";
         }
+
+        public override bool IsValidDestination(Position origin, Position destination, ChessGame game)
+        {
+            Utilities.ThrowIfNull(origin, "origin");
+            Utilities.ThrowIfNull(destination, "destination");
+            PositionDistance posDelta = new PositionDistance(origin, destination);
+            if (posDelta.DistanceX != posDelta.DistanceY)
+                return false;
+            bool increasingRank = (int)destination.Rank > (int)origin.Rank;
+            bool increasingFile = (int)destination.File > (int)origin.File;
+            ChessPiece[][] board = game.GetBoardForReadOnlyPurposes();
+            for (int f = (int)origin.File + (increasingFile ? 1 : -1), r = (int)origin.Rank + (increasingRank ? 1 : -1);
+                 increasingFile ? f < (int)destination.File : f > (int)destination.File;
+                 f += increasingFile ? 1 : -1, r += increasingRank ? 1 : -1)
+            {
+                if (board[r][f].Owner != Player.None)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
 }
