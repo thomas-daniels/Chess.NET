ced0f8bf47d6f2875e7f225d1f6c542ba24b4460
ProgramFOX <programfox@hotmail.be>
2016-01-22 20:34:32 +0100

    Implement IsValidDestination for Rook

--- ChessDotNet/Pieces/Rook.cs
+++ ChessDotNet/Pieces/Rook.cs
@@ -1,4 +1,6 @@
-﻿namespace ChessDotNet.Pieces
+﻿using System;
+
+namespace ChessDotNet.Pieces
 {
     public class Rook : ChessPiece
     {
@@ -16,6 +18,45 @@
         public override string GetFenCharacter()
         {
             return Owner == Player.White ? "R" : "r";
+        }
+
+        public override bool IsValidDestination(Position origin, Position destination, ChessGame game)
+        {
+            Utilities.ThrowIfNull(origin, "origin");
+            Utilities.ThrowIfNull(destination, "destination");
+
+            PositionDistance posDelta = new PositionDistance(origin, destination);
+            if (posDelta.DistanceX != 0 && posDelta.DistanceY != 0)
+                return false;
+            bool increasingRank = (int)destination.Rank > (int)origin.Rank;
+            bool increasingFile = (int)destination.File > (int)origin.File;
+            if (posDelta.DistanceX == 0)
+            {
+                int f = (int)origin.File;
+                for (int r = (int)origin.Rank + (increasingRank ? 1 : -1);
+                    increasingRank ? r < (int)destination.Rank : r > (int)destination.Rank;
+                    r += increasingRank ? 1 : -1)
+                {
+                    if (game.GetPieceAt((File)f, (Rank)r).Owner != Player.None)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else // (posDelta.DeltaY == 0)
+            {
+                int r = (int)origin.Rank;
+                for (int f = (int)origin.File + (increasingFile ? 1 : -1);
+                    increasingFile ? f < (int)destination.File : f > (int)destination.File;
+                    f += increasingFile ? 1 : -1)
+                {
+                    if (game.GetPieceAt((File)f, (Rank)r).Owner != Player.None)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
     }
 }
