2b8bd03dd55e768aa28340450e8803c79a744544
ProgramFOX <programfox@hotmail.be>
2016-01-22 19:54:04 +0100

    Make ChessPiece abstract and remove obsolete funcs

--- ChessDotNet/ChessPiece.cs
+++ ChessDotNet/ChessPiece.cs
@@ -1,33 +1,12 @@
-﻿using System.Globalization;
-
-namespace ChessDotNet
+﻿namespace ChessDotNet
 {
-    public class ChessPiece
+    public abstract class ChessPiece
     {
-        public Piece Piece
-        {
-            get;
-            private set;
-        }
 
         public Player Player
         {
             get;
             private set;
-        }
-
-        public static ChessPiece None
-        {
-            get
-            {
-                return new ChessPiece(Piece.None, Player.None);
-            }
-        }
-
-        public ChessPiece(Piece piece, Player player)
-        {
-            Piece = piece;
-            Player = player;
         }
 
         public override bool Equals(object obj)
@@ -38,12 +17,12 @@ namespace ChessDotNet
                 return false;
             ChessPiece piece1 = this;
             ChessPiece piece2 = (ChessPiece)obj;
-            return piece1.Piece == piece2.Piece && piece1.Player == piece2.Player;
+            return piece1.Player == piece2.Player;
         }
 
         public override int GetHashCode()
         {
-            return new { Piece, Player }.GetHashCode();
+            return new { Piece = GetFenCharacter(), Player }.GetHashCode();
         }
 
         public static bool operator ==(ChessPiece piece1, ChessPiece piece2)
@@ -64,9 +43,6 @@ namespace ChessDotNet
             return !piece1.Equals(piece2);
         }
 
-        public override string ToString()
-        {
-            return string.Format(CultureInfo.InvariantCulture, "ChessPiece: {0}, {1}", Piece, Player);
-        }
+        public abstract string GetFenCharacter();
     }
 }
