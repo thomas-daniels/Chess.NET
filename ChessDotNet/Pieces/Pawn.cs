a353d7ca53e1fadd1c26b3cc3ba4878b94ce4099
ProgramFOX <programfox@hotmail.be>
2016-01-23 14:25:23 +0100

    Implement IsValidDestination for Pawn

--- ChessDotNet/Pieces/Pawn.cs
+++ ChessDotNet/Pieces/Pawn.cs
@@ -1,4 +1,7 @@
-﻿namespace ChessDotNet.Pieces
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ChessDotNet.Pieces
 {
     public class Pawn : ChessPiece
     {
@@ -16,6 +19,84 @@
         public override string GetFenCharacter()
         {
             return Owner == Player.White ? "P" : "p";
+        }
+
+        public override bool IsValidDestination(Position origin, Position destination, ChessGame game)
+        {
+            Utilities.ThrowIfNull(origin, "origin");
+            Utilities.ThrowIfNull(destination, "destination");
+            Utilities.ThrowIfNull(game, "game");
+            ChessPiece promotion = null; // TODO: pass as argument
+            PositionDistance posDelta = new PositionDistance(origin, destination);
+            if ((posDelta.DistanceX != 0 || posDelta.DistanceY != 1) && (posDelta.DistanceX != 1 || posDelta.DistanceY != 1)
+                        && (posDelta.DistanceX != 0 || posDelta.DistanceY != 2))
+                return false;
+            if (Owner == Player.White)
+            {
+                if ((int)origin.Rank < (int)destination.Rank)
+                    return false;
+                if (destination.Rank == Rank.Eight && promotion == null)
+                    return false;
+            }
+            if (Owner == Player.Black)
+            {
+                if ((int)origin.Rank > (int)destination.Rank)
+                    return false;
+                if (destination.Rank == Rank.One && promotion == null)
+                    return false;
+            }
+            bool checkEnPassant = false;
+            if (posDelta.DistanceY == 2)
+            {
+                if ((origin.Rank != Rank.Two && Owner == Player.White)
+                    || (origin.Rank != Rank.Seven && Owner == Player.Black))
+                    return false;
+                if (origin.Rank == Rank.Two && game.GetPieceAt(origin.File, Rank.Three) != null)
+                    return false;
+                if (origin.Rank == Rank.Seven && game.GetPieceAt(origin.File, Rank.Six) != null)
+                    return false;
+            }
+            if (posDelta.DistanceX == 0 && (posDelta.DistanceY == 1 || posDelta.DistanceY == 2))
+            {
+                if (game.GetPieceAt(destination).Owner != Player.None)
+                    return false;
+            }
+            else
+            {
+                if (game.GetPieceAt(destination).Owner != Utilities.GetOpponentOf(Owner))
+                    checkEnPassant = true;
+                if (game.GetPieceAt(destination).Owner == Owner)
+                    return false;
+            }
+            if (checkEnPassant)
+            {
+                ReadOnlyCollection<DetailedMove> _moves = game.Moves;
+                if (_moves.Count == 0)
+                {
+                    return false;
+                }
+                if ((origin.Rank != Rank.Five && Owner == Player.White)
+                    || (origin.Rank != Rank.Four && Owner == Player.Black))
+                    return false;
+                Move latestMove = _moves[_moves.Count - 1];
+                if (latestMove.Player != Utilities.GetOpponentOf(Owner))
+                    return false;
+                if (game.GetPieceAt(latestMove.NewPosition) != null)
+                    return false;
+                if (Owner == Player.White)
+                {
+                    if (latestMove.OriginalPosition.Rank != Rank.Seven || latestMove.NewPosition.Rank != Rank.Five)
+                        return false;
+                }
+                else // (m.Player == Players.Black)
+                {
+                    if (latestMove.OriginalPosition.Rank != Rank.Two || latestMove.NewPosition.Rank != Rank.Four)
+                        return false;
+                }
+                if (destination.File != latestMove.NewPosition.File)
+                    return false;
+            }
+            return true;
         }
     }
 }
