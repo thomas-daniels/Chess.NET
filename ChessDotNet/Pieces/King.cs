2d28cebdb6c7b84a2e45a4482f0c515c56db40ba
ProgramFOX <programfox@hotmail.be>
2016-01-23 22:12:37 +0100

    Implement GetValidMoves for King

--- ChessDotNet/Pieces/King.cs
+++ ChessDotNet/Pieces/King.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace ChessDotNet.Pieces
 {
@@ -102,6 +104,32 @@ namespace ChessDotNet.Pieces
         public override float GetRelativePieceValue()
         {
             return float.PositiveInfinity;
+        }
+
+        public override ReadOnlyCollection<Move> GetValidMoves(Position from, bool returnIfAny, ChessGame game)
+        {
+            Utilities.ThrowIfNull(from, "from");
+            List<Move> validMoves = new List<Move>();
+            Piece piece = game.GetPieceAt(from);
+            Piece[][] board = game.GetBoard();
+            int l0 = board.Length;
+            int l1 = board[0].Length;
+            int[][] directions = new int[][] { new int[] { 0, 1 }, new int[] { 1, 0 }, new int[] { 0, -1 }, new int[] { -1, 0 },
+                        new int[] { 1, 1 }, new int[] { 1, -1 }, new int[] { -1, 1 }, new int[] { -1, -1 } };
+            foreach (int[] dir in directions)
+            {
+                if ((int)from.File + dir[0] < 0 || (int)from.File + dir[0] >= l1
+                    || (int)from.Rank + dir[1] < 0 || (int)from.Rank + dir[1] >= l0)
+                    continue;
+                Move move = new Move(from, new Position(from.File + dir[0], from.Rank + dir[1]), piece.Owner);
+                if (game.IsValidMove(move))
+                {
+                    validMoves.Add(move);
+                    if (returnIfAny)
+                        return new ReadOnlyCollection<Move>(validMoves);
+                }
+            }
+            return new ReadOnlyCollection<Move>(validMoves);
         }
     }
 }
