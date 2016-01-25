130b5fac4636040c3b81818b1f776659697baa91
ProgramFOX <programfox@hotmail.be>
2016-01-25 19:41:25 +0100

    Implement surrounding capture rule for Atomic

--- ChessDotNet.Variants/Atomic/AtomicChessGame.cs
+++ ChessDotNet.Variants/Atomic/AtomicChessGame.cs
@@ -1,6 +1,27 @@
 ﻿namespace ChessDotNet.Variants.Atomic
 {
+    using Pieces;
+
     public class AtomicChessGame : ChessGame
     {
+        public override MoveType ApplyMove(Move move, bool alreadyValidated)
+        {
+            MoveType type = base.ApplyMove(move, alreadyValidated);
+            if (!type.HasFlag(MoveType.Capture))
+                return type;
+            int[][] surroundingSquares = new int[][] { new int[] { 1, 0 }, new int[] { 1, 1 }, new int[] { 0, 1 }, new int[] { -1, -1 },
+                                                       new int[] { -1, 0 }, new int[] { 0, -1 }, new int[] { -1, 1 }, new int[] { 1, -1 } };
+            SetPieceAt(move.NewPosition.File, move.NewPosition.Rank, null);
+            foreach (int[] surroundingSquaresDistance in surroundingSquares)
+            {
+                File f = move.NewPosition.File + surroundingSquaresDistance[0];
+                Rank r = move.NewPosition.Rank + surroundingSquaresDistance[1];
+                if (!(GetPieceAt(f, r) is Pawn))
+                {
+                    SetPieceAt(f, r, null);
+                }
+            }
+            return type;
+        }
     }
 }
