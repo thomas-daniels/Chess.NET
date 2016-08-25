575054967fad11b919d6b5a3573081f6422157d2
ProgramFOX <programfox@hotmail.be>
2016-08-25 10:38:57 +0200

    Add RacingKingsChessGame and tests

---
+++ ChessDotNet.Variants.Tests/RacingKingsChessGameTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ChessDotNet.Variants.Tests
+{
+    using RacingKings;
+
+    [TestFixture]
+    public class RacingKingsChessGameTests
+    {
+        [Test]
+        public static void TestStartPosition()
+        {
+            RacingKingsChessGame game = new RacingKingsChessGame();
+            Assert.AreEqual("8/8/8/8/8/8/krbnNBRK/qrbnNBRQ w - - 0 1", game.GetFen());
+        }
+
+        [Test]
+        public static void TestInvalidMove_NoCheck()
+        {
+            RacingKingsChessGame game = new RacingKingsChessGame();
+            Assert.False(game.IsValidMove(new Move("E2", "C1", Player.White)));
+        }
+
+        [Test]
+        public static void TestGetValidMoves()
+        {
+            RacingKingsChessGame game = new RacingKingsChessGame();
+            Assert.AreEqual(21, game.GetValidMoves(Player.White).Count);
+        }
+
+        [Test]
+        public static void TestIsWinnerWhite1()
+        {
+            RacingKingsChessGame game = new RacingKingsChessGame("5K2/1k6/8/8/8/8/1rbnNBR1/qrbnNBRQ b - - 11 6");
+            Assert.False(game.IsWinner(Player.White));
+
+            game.ApplyMove(new Move("B7", "C7", Player.Black), true);
+            Assert.True(game.IsWinner(Player.White));
+        }
+
+        [Test]
+        public static void TestIsWinnerWhite2()
+        {
+            RacingKingsChessGame game = new RacingKingsChessGame("5K2/1k6/8/8/8/8/1rbnNBR1/qrbnNBRQ b - - 11 6");
+            Assert.False(game.IsWinner(Player.White));
+
+            game.ApplyMove(new Move("B7", "B8", Player.Black), true);
+            Assert.False(game.IsWinner(Player.White));
+        }
+
+        [Test]
+        public static void TestIsWinnerBlack()
+        {
+            RacingKingsChessGame game = new RacingKingsChessGame("2k5/5K2/8/8/8/8/1rbnNBR1/qrbnNBRQ w - - 12 7");
+            Assert.True(game.IsWinner(Player.Black));
+        }
+    }
+}
---
+++ ChessDotNet.Variants/RacingKings/RacingKingsChessGame.cs
@@ -0,0 +1,148 @@
+﻿using ChessDotNet.Pieces;
+using ChessDotNet.Variants.RacingKings.Pieces;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ChessDotNet.Variants.RacingKings
+{
+    public class RacingKingsChessGame : ChessGame
+    {
+        protected override bool CastlingCanBeLegal
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        private Dictionary<char, Piece> fenMappings = new Dictionary<char, Piece>()
+        {
+            { 'K', new RacingKingsKing(Player.White) },
+            { 'k', new RacingKingsKing(Player.Black) },
+            { 'Q', new Queen(Player.White) },
+            { 'q', new Queen(Player.Black) },
+            { 'R', new Rook(Player.White) },
+            { 'r', new Rook(Player.Black) },
+            { 'B', new Bishop(Player.White) },
+            { 'b', new Bishop(Player.Black) },
+            { 'N', new Knight(Player.White) },
+            { 'n', new Knight(Player.Black) },
+            { 'P', new Pawn(Player.White) },
+            { 'p', new Pawn(Player.Black) },
+        };
+        protected override Dictionary<char, Piece> FenMappings
+        {
+            get
+            {
+                return fenMappings;
+            }
+        }
+
+        public RacingKingsChessGame()
+        {
+            WhoseTurn = Player.White;
+            Moves = new ReadOnlyCollection<DetailedMove>(new List<DetailedMove>());
+            Board = new Piece[8][];
+            Piece kb = FenMappings['k'];
+            Piece qb = FenMappings['q'];
+            Piece rb = FenMappings['r'];
+            Piece nb = FenMappings['n'];
+            Piece bb = FenMappings['b'];
+            Piece kw = FenMappings['K'];
+            Piece qw = FenMappings['Q'];
+            Piece rw = FenMappings['R'];
+            Piece bw = FenMappings['B'];
+            Piece nw = FenMappings['N'];
+            Piece o = null;
+            Board = new Piece[8][]
+            {
+                new[] { o, o, o, o, o, o, o, o },
+                new[] { o, o, o, o, o, o, o, o },
+                new[] { o, o, o, o, o, o, o, o },
+                new[] { o, o, o, o, o, o, o, o },
+                new[] { o, o, o, o, o, o, o, o },
+                new[] { o, o, o, o, o, o, o, o },
+                new[] { kb, rb, bb, nb, nw, bw, rw, kw },
+                new[] { qb, rb, bb, nb, nw, bw, rw, qw }
+            };
+            CanBlackCastleKingSide = CanBlackCastleQueenSide = CanWhiteCastleKingSide = CanWhiteCastleQueenSide = false;
+        }
+        public RacingKingsChessGame(Piece[][] board, Player whoseTurn) : base(board, whoseTurn) { }
+        public RacingKingsChessGame(GameCreationData data) : base(data) { }
+        public RacingKingsChessGame(string fen) : base(fen) { }
+        public RacingKingsChessGame(IEnumerable<Move> moves, bool movesAreValidated) : base(moves, movesAreValidated) { }
+
+        protected override bool IsValidMove(Move move, bool validateCheck, bool careAboutWhoseTurnItIs)
+        {
+            bool validAsPerStandardRules = base.IsValidMove(move, validateCheck, careAboutWhoseTurnItIs);
+            if (!validAsPerStandardRules) return false;
+            if (validateCheck && WouldBeInCheckAfter(move, ChessUtilities.GetOpponentOf(move.Player)))
+            {
+                return false;
+                // In racing kings, you can't check(mate) your opponent.
+            }
+
+            return true;
+        }
+
+        protected bool IsKingOnBackRank(Player player)
+        {
+            for (int i = 0; i < BoardWidth; i++)
+            {
+                Piece p = Board[0][i];
+                if (p is King && p.Owner == player)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        protected Position FindKing(Player player)
+        {
+            for (int r = 0; r < BoardHeight; r++)
+            {
+                for (int f = 0; f < BoardWidth; f++)
+                {
+                    if (Board[r][f] is King && Board[r][f].Owner == player)
+                    {
+                        return new Position((File)f, 8 - r);
+                    }
+                }
+            }
+            return null;
+        }
+
+        public override bool IsWinner(Player player)
+        {
+            bool kingOnBackRank = IsKingOnBackRank(player);
+            if (!kingOnBackRank) return kingOnBackRank;
+
+            if (player == Player.Black)
+            {
+                return true;
+            }
+            else if (WhoseTurn == Player.White)
+            {
+                if (FindKing(Player.Black).Rank != 8)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                ReadOnlyCollection<Move> validKingMoves = GetValidMoves(FindKing(Player.Black));
+                if (validKingMoves.Any(x => x.NewPosition.Rank == 8))
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
